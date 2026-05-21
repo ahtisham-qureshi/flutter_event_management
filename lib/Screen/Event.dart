7b6e69f97fb409367d040bc2570ada608915cba2
Mohammad Ahtisham Qureshi <maq.06169@gmail.com>
2026-05-21 15:03:40 +0530

    Save Data in SharedPreferences

--- lib/Screen/Event.dart
+++ lib/Screen/Event.dart
@@ -4,4 +4,8 @@ class Event {
   String location;
 
   Event({required this.title, required this.date, required this.location});
+
+  Map<String, dynamic> toJson() {
+    return {"title": title, "date": date, "location": location};
+  }
 }
